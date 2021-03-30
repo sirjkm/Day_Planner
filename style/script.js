a1ac1c4b9111b25572e530c598a39b2800295dbc
Jason Manning <jasonkylemanning@gmail.com>
2021-03-29 23:35:58 -0400

    update HTML button ids and JS functions

--- style/script.js
+++ style/script.js
@@ -1,1 +1,16 @@
-var myTextBox = document.querySelector('.activity');
+var calendar = document.getElementById("calendar")
+
+document.getElementById("btn_1").addEventListener("click",
+
+    function() {
+        console.log(document.getElementById("activity_1").value)
+
+        localStorage.setItem("activity_1", document.getElementById("activity_1").value)
+    });
+
+    function loadData(activity_num) {
+        var activity = localStorage.getItem('activity_' + activity_num)
+        document.getElementById('activity_' + activity_num).value = activity;
+    }
+
+    loadData(1)
